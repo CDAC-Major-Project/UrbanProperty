b2b7ccbcbf750e84dd4c9966504ccec86518c8a7
Abhay-yadav966 <yadavabhay966@gmail.com>
2025-08-08 00:28:19 +0530

    Quick Link from admin dashboard has been created

--- src/components/core/Admin/Analytics.jsx
+++ src/components/core/Admin/Analytics.jsx
@@ -17,7 +17,10 @@ import PeopleAltOutlinedIcon from "@mui/icons-material/PeopleAltOutlined";
 import CorporateFareOutlinedIcon from "@mui/icons-material/CorporateFareOutlined";
 import ErrorOutlineOutlinedIcon from "@mui/icons-material/ErrorOutlineOutlined";
 import CountUp from "react-countup";
-import { colors } from "@mui/material";
+import TaskAltOutlinedIcon from "@mui/icons-material/TaskAltOutlined";
+import RemoveRedEyeOutlinedIcon from "@mui/icons-material/RemoveRedEyeOutlined";
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import ManageAccountsOutlinedIcon from '@mui/icons-material/ManageAccountsOutlined';
 
 const Analytics = () => {
   const response = {
@@ -190,42 +193,105 @@ const Analytics = () => {
         </ResponsiveContainer>
       </div>
 
-      {/* Pie Chart */}
-      <div className="border-2 border-gray-300 bg-white rounded-2xl shadow-gray-300 shadow-lg p-5 w-fit ">
-        <h3 className="text-lg font-medium ">Property Verification Status</h3>
-        <p className="text-sm text-gray-600">
-          Distribution of property verification statuses
-        </p>
-       
-        <div className="flex items-center justify-center ">
-          <PieChart width={400} height={400}>
-            <Pie
-              data={PieData}
-              innerRadius={100}
-              outerRadius={130}
-              fill="#8884d8"
-              paddingAngle={2}
-              dataKey="value"
-              labelLine={false}
-            >
-              {PieData?.map((data, index) => (
-                <Cell
-                  key={data?.name}
-                  fill={PieColors[index % PieColors?.length]}
-                />
-              ))}
-            </Pie>
-            <Tooltip formatter={(value= Number, name= String) => [`${value}`, name]} />
-            <Legend
-              layout="horizontal"
-              verticalAlign="bottom"
-              iconType="circle"
-              align="center"
-              formatter={(value, entry, index) =>
-                `${value}: ${PieData[index]?.value}`
-              }
-            />
-          </PieChart>
+      <div className="flex space-x-5">
+        {/* Pie Chart */}
+        <div className="border-2 border-gray-300 bg-white rounded-2xl shadow-gray-300 shadow-lg p-5 w-fit ">
+          <h3 className="text-lg font-medium ">Property Verification Status</h3>
+          <p className="text-sm text-gray-600">
+            Distribution of property verification statuses
+          </p>
+
+          <div className="flex items-center justify-center ">
+            <PieChart width={400} height={400}>
+              <Pie
+                data={PieData}
+                innerRadius={100}
+                outerRadius={130}
+                fill="#8884d8"
+                paddingAngle={2}
+                dataKey="value"
+                labelLine={false}
+              >
+                {PieData?.map((data, index) => (
+                  <Cell
+                    key={data?.name}
+                    fill={PieColors[index % PieColors?.length]}
+                  />
+                ))}
+              </Pie>
+              <Tooltip
+                formatter={(value = Number, name = String) => [
+                  `${value}`,
+                  name,
+                ]}
+              />
+              <Legend
+                layout="horizontal"
+                verticalAlign="bottom"
+                iconType="circle"
+                align="center"
+                formatter={(value, entry, index) =>
+                  `${value}: ${PieData[index]?.value}`
+                }
+              />
+            </PieChart>
+          </div>
+        </div>
+
+        <div className="w-1/2 space-y-5 ">
+          {/* Verify Property Link */}
+          <div className="border-2 border-gray-300 bg-white rounded-2xl shadow-gray-300 shadow-lg p-5 w-full flex items-center justify-between ">
+            {/* Icon and Heading */}
+            <div className="flex items-center space-x-3">
+              <TaskAltOutlinedIcon sx={{ color: "#00A63E" }} fontSize="large" />
+              <div>
+                <h3 className="font-semibold">Verify Properties</h3>
+                <p className="text-sm text-gray-600">89 pending verification</p>
+              </div>
+            </div>
+
+            {/* button */}
+            <button className=" cursor-pointer flex items-center space-x-2 border border-gray-300 rounded-lg py-1 px-5 ">
+              <RemoveRedEyeOutlinedIcon fontSize="small" />
+              <p>Review Now</p>
+            </button>
+          </div>
+
+          {/* Property Type Link */}
+          <div className="border-2 border-gray-300 bg-white rounded-2xl shadow-gray-300 shadow-lg p-5 w-full flex items-center justify-between ">
+            {/* Icon and Heading */}
+            <div className="flex items-center space-x-3">
+              <CorporateFareOutlinedIcon sx={{ color: "#155DFC" }} fontSize="large" />
+              <div>
+                <h3 className="font-semibold">Property Type</h3>
+                <p className="text-sm text-gray-600">Manage property categories</p>
+              </div>
+            </div>
+
+            {/* button */}
+            <button className=" cursor-pointer flex items-center space-x-2 border border-gray-300 rounded-lg py-1 px-5 ">
+              <SettingsOutlinedIcon fontSize="small" />
+              <p>Manage Types</p>
+            </button>
+          </div>
+
+          {/* User Management Link */}
+          <div className="border-2 border-gray-300 bg-white rounded-2xl shadow-gray-300 shadow-lg p-5 w-full flex items-center justify-between ">
+            {/* Icon and Heading */}
+            <div className="flex items-center space-x-3">
+              <PeopleAltOutlinedIcon sx={{ color: "#9810FA" }} fontSize="large" />
+              <div>
+                <h3 className="font-semibold">User Management</h3>
+                <p className="text-sm text-gray-600">1,234 total users</p>
+              </div>
+            </div>
+
+            {/* button */}
+            <button className=" cursor-pointer flex items-center space-x-2 border border-gray-300 rounded-lg py-1 px-5 ">
+              <ManageAccountsOutlinedIcon fontSize="small" />
+              <p>Manage Users</p>
+            </button>
+          </div>
         </div>
       </div>
     </div>
